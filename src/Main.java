8bf32211c288a3bdcd5901fe79622834a5d3606a
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2022-08-08 20:44:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         int year = 2021;
-       System.out.println(checkTheLeapYear(year));
+        checkTheLeapYear(year);
 
         int clientOS = 0;
         int currentYear = LocalDate.now().getYear();
@@ -17,27 +17,30 @@ public class Main {
 
     //task 1
     private static String checkTheLeapYear(int year) {
-      String strOut;
         if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)) {
-            strOut = year + " - не високосный год";
+            System.out.println(year + " - не високосный год");
         } else {
-            strOut = year + " - високосный год";
+           System.out.println(year + " - високосный год");
         }
         return strOut;
     }
+    
     //task 2
     private static String checkPhonesOS(int clientOS, int currentYear) {
-        String strComment;
+        int iOs = 0;
+        int android = 1;
+        
         if (clientOS == 0 && currentYear >= 2015 && currentYear < 2023) {
-            strComment = "Установите версию приложения для iOS по ссылке";
+           System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1 && currentYear >= 2015 && currentYear < 2023) {
-            strComment = "Установите версию приложения для Android по ссылке";
+            System.out.println("Установите версию приложения для Android по ссылке");
         } else if (clientOS == 0 && currentYear < 2015) {
-            strComment = "Установите облегченную версию приложения для iOS по ссылке";
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 1 && currentYear < 2015) {
-            strComment = "Установите облегченную версию приложения для Android по ссылке";
-        } else throw new RuntimeException("Введены недопустимые параметры ОС или год выпуска");
-        return strComment;
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else throw new {
+            RuntimeException("Введены недопустимые параметры ОС или год выпуска");
+        }
     }
 
     //task 3
