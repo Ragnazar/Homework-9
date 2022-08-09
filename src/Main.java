756529371008bb1a3c16d306603a4c053fe660a8
Ragnazar <avdeyev.iv@gmail.com>
2022-08-09 11:48:20 +0300

    fix-conversations

--- src/Main.java
+++ src/Main.java
@@ -7,58 +7,58 @@ public class Main {
 
         int clientOS = 0;
         int currentYear = LocalDate.now().getYear();
-        System.out.println(checkPhonesOS(clientOS, currentYear));
+        checkPhonesOS(clientOS, currentYear);
 
-        int deliveryDistance = 95;
-        System.out.println("Потребуется дней: " +  countDeliveryDays(deliveryDistance));
+        int deliveryDistance = 99;
+        System.out.println("Потребуется дней: " + countDeliveryDays(deliveryDistance));
 
 
     }
 
     //task 1
-    private static String checkTheLeapYear(int year) {
+    private static void  checkTheLeapYear(int year) {
         if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)) {
             System.out.println(year + " - не високосный год");
         } else {
-           System.out.println(year + " - високосный год");
+            System.out.println(year + " - високосный год");
         }
-        return strOut;
     }
-    
+
     //task 2
-    private static String checkPhonesOS(int clientOS, int currentYear) {
+    private static void checkPhonesOS(int clientOS, int currentYear) {
         int iOs = 0;
         int android = 1;
-        
-        if (clientOS == 0 && currentYear >= 2015 && currentYear < 2023) {
-           System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && currentYear >= 2015 && currentYear < 2023) {
+        int minSupported = 2015;
+        int nextYear = 2023;
+
+        if (clientOS == iOs && currentYear >= minSupported && currentYear < nextYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == android && currentYear >= minSupported && currentYear < nextYear) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 0 && currentYear < 2015) {
+        } else if (clientOS == iOs && currentYear < minSupported) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && currentYear < 2015) {
+        } else if (clientOS == android && currentYear < minSupported) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else throw new {
-            RuntimeException("Введены недопустимые параметры ОС или год выпуска");
-        }
+        } else throw new
+                RuntimeException("Введены недопустимые параметры ОС или год выпуска");
+
     }
 
     //task 3
     private static Integer countDeliveryDays(int deliveryDistance) {
-        int daysRequired = 2;
+        int daysRequired = 1;
         int firstBorder = 20;
         int secondBorder = 60;
         int lastBorder = 100;
 
         if (deliveryDistance <= firstBorder) {
-            --daysRequired;
+            return  daysRequired;
+        } else if (deliveryDistance <= secondBorder) {
+           return  ++daysRequired;
+        } else if (deliveryDistance <= lastBorder) {
+           return daysRequired + 2;
         } else {
-            if (deliveryDistance <= secondBorder) {
-            } else if (deliveryDistance <= lastBorder) {
-                 ++daysRequired;
-            } else {
-                throw new RuntimeException("Вне зоны доставки");
-            }
-        } return daysRequired;
-    }
+            throw new RuntimeException("Вне зоны доставки");
+        }
+}
 }
